586ddd0a2a5e129a7834cd9c9922996e4b0814be
Ilya Kharlamov <ikharlamov@griddynamics.com>
2018-01-26 14:11:36 +0300

    update jenkins image

--- jenkins/files/opt/start-jenkins.sh
+++ jenkins/files/opt/start-jenkins.sh
@@ -2,8 +2,10 @@
 echo "================"
 echo "Starting jenkins"
 echo "================"
+cp -r /opt/init.groovy.d /opt/jenkins/
 chown -R jenkins:jenkins /opt/jenkins/
 su jenkins -c '/usr/bin/java \
+               -Djenkins.install.runSetupWizard=false \
                -Dhudson.model.DirectoryBrowserSupport.CSP="" \
                -Xmx512m \
                -jar /opt/jenkins.war'
